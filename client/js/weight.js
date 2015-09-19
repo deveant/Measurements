3881a784204f0a500d6386cb50a467bf47a20ee8
deveant <ste03_aus@hotmail.com>
2015-09-19 10:33:27 +1000

    Added positive sign on positive weight displayed in weight.html

--- client/js/weight.js
+++ client/js/weight.js
@@ -1,5 +1,8 @@
 Template.weight.helpers({ 
      createdAtFormatted: function () { 
        return moment(this.createdAt).format('DD/MM (ddd)'); 
-     } 
+     },
+     positiveDifference: function(difference) {
+    	return (difference > 0 ? '+' : '');
+    }
  }); 
